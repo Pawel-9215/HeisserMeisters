d6412041acc086e17076b5a4cc5ec2e14d7fd1e2
Pawel-9215 <hordyniak@gmail.com>
2021-01-23 10:27:26 +0100

    light

--- Player.gd
+++ Player.gd
@@ -5,6 +5,7 @@ var motion = Vector2()
 func _physics_process(delta):
 	update_movement()
 	move_and_slide(motion)
+	torch_switch()
 
 func update_movement():
 	look_at(get_global_mouse_position())
@@ -21,3 +22,7 @@ func update_movement():
 		motion.x = clamp(motion.x + SPEED, 0, MAX_SPEED)
 	else:
 		motion.x = lerp(motion.x, 0, FRICTION)
+		
+func torch_switch():
+	if Input.is_action_just_pressed("flashlight"):
+		$Torch.enabled = !$Torch.enabled
